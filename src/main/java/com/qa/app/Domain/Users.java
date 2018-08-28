b9bc2ae9c536fb7560e9399ed1cf40f958b9ed27
Framsida <Joshuastait@gmail.com>
2018-08-28 17:18:22 +0100

    Initial EE setup complete. Added config files.

---
+++ src/main/java/com/qa/app/Domain/Users.java
@@ -0,0 +1,57 @@
+package com.qa.app.Domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    public Users() {
+    }
+
+    public Users(String firstname, String lastName, String accountNumber) {
+        this.firstName = firstname;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
